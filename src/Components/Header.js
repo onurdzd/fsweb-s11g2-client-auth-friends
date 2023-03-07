1f2e4135e81fdb85f33e1129adf3cb25f5a832ff
onurd <106928484+onurdzd@users.noreply.github.com>
2023-03-07 22:08:22 +0300

    friendslist ,addfriend,logout,login aktif.private route eksik

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,24 +1,39 @@
-import React from 'react';
-import { useHistory } from 'react-router-dom';
+import React from "react";
+import { useHistory } from "react-router-dom";
+
 
 const Header = () => {
+  const history = useHistory();
 
-    const history=useHistory()
-    return (
+  const logOut = () => {
+    history.push("/");
+    // axiosWithAuth().post('http://localhost:9000/api/logout').then(res=>{
+    //    console.log(res)
+    // })
+    localStorage.removeItem("token");
+  };
+
+  return (
+    <div>
+      <div className="loginFormHeaderDiv">
         <div>
-            <div className='loginFormHeaderDiv'>
-            <div>
-            <h1>FRIENDS DATABASE</h1>
-            </div>
-            <div className='loginFormHeaderButtonDiv'>
-            <button onClick={()=> history.push("/login")}>LOGIN.</button>
-            <button onClick={()=> history.push("/friendList")}>FRIENDLIST.</button>
-            <button >ADDFRIEND.</button>
-            <button onClick={()=> history.push("/")}>LOGOUT</button>
-            </div>
+          <h1>FRIENDS DATABASE</h1>
         </div>
+        <div className="loginFormHeaderButtonDiv">
+          {!localStorage.getItem("token") && (
+            <button onClick={() => history.push("/login")}>LOGIN.</button>
+          )}
+          <button onClick={() => history.push("/friendList")}>
+            FRIENDLIST.
+          </button>
+          <button onClick={() => history.push("/addFriend")}>ADDFRIEND.</button>
+          {localStorage.getItem("token") && (
+            <button onClick={logOut}>LOGOUT</button>
+          )}
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Header;
